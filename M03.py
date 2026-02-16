a203ee7a23e100cfb2d677eb2255df7b9d36d7cb
Sai-K-Kham <skham1@ivytech.edu>
2026-02-15 21:27:02 -0500

    Unit test

--- M03.py
+++ M03.py
@@ -1,11 +1,29 @@
 #Sai Khay Kham
 #Captalize elements in the list
 class Cap:
-    def cap(things, elem):
-        things = ["mozzarella","cinderalla","salmonella"]
+    def cap(things, match):
         counter = 0
         for thing in things:
-            if thing == "cinderalla":
+            if thing == match:
                 print(things[counter].capitalize())
                 print(things)
             counter += 1
+
+if __name__ == "__main__":
+    things = ["mozzarella","cinderalla","salmonella"]
+    match = "cinderalla"
+    Cap.cap(things, match)
+    match = "mozzarella"
+    Cap.cap(things, match)
+    match = "salmonella"
+    Cap.cap(things, match)
+#output
+#Cinderalla
+#['mozzarella', 'cinderalla', 'salmonella']
+#Mozzarella
+#['mozzarella', 'cinderalla', 'salmonella']
+#Salmonella
+#['mozzarella', 'cinderalla', 'salmonella']
+
+#with capitalize() method, it only captalize when the method is called.
+#but not captalized the element in the list itself.
